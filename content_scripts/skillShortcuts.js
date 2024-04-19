6b15b1bd58ba7067b400ca15e415880cbbf655d2
TranTam <tamvaa1306@gmail.com>
2024-04-19 10:35:36 +0700

    remove console.log

--- content_scripts/skillShortcuts.js
+++ content_scripts/skillShortcuts.js
@@ -1,7 +1,6 @@
 const findDrawerContent = setInterval(() => {
   const drawerContent = document.querySelector(".drawer-content");
   if (drawerContent) {
-    console.log(drawerContent);
     clearInterval(findDrawerContent);
     const spellEls = drawerContent.querySelectorAll(
       ".spell-container div[id^='spell_']"
